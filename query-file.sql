8506045d2dc60569f5a50defb00e4cbb0213240e
SriThriveni <145109645+SriThriveni@users.noreply.github.com>
2025-10-21 18:26:17 +0530

    Update query-file.sql

--- query-file.sql
+++ query-file.sql
@@ -1,35 +1,39 @@
 
-USE library1_db;
+CREATE DATABASE library_db;
+USE library_db;
 
-CREATE TABLE Author(
-author_id INT AUTO_INCREMENT PRIMARY KEY,
-namee VARCHAR(100) NOT NULL,
-country VARCHAR(50)
-);
-CREATE TABLE Book(
-book_id INT AUTO_INCREMENT PRIMARY KEY ,
-title VARCHAR(100) NOT NULL,
-author_id INT ,
-publication_year INT,
-FOREIGN KEY (author_id) REFERENCES Author(author_id)
-);
-
-CREATE TABLE IF NOT EXISTS Member (
-member_id INT AUTO_INCREMENT PRIMARY KEY ,
-namee VARCHAR(100) NOT NULL,
-email VARCHAR(100) UNIQUE,
-join_date DATE
+CREATE TABLE author (
+   author_id INT AUTO_INCREMENT PRIMARY KEY ,
+   name VARCHAR(100) NOT NULL,
+   country VARCHAR(50)
+   );
+CREATE TABLE book (
+    book_id INT AUTO_INCREMENT PRIMARY KEY ,
+    title VARCHAR(100) NOT NULL,
+    author_id INT,
+    publication_year YEAR,
+    FOREIGN KEY (author_id) REFERENCES author(author_id)
+           ON DELETE SET NULL
+           ON UPDATE CASCADE
 );
 
-CREATE TABLE IF NOT EXISTS Loan(
-loan_id INT AUTO_INCREMENT PRIMARY KEY ,
-book_id INT,
-member_id INT,
-issue_date DATE,
-return_date DATE,
-FOREIGN KEY (book_id) REFERENCES Book(book_id),
-FOREIGN KEY (member_id) REFERENCES Member(member_id)
+CREATE TABLE member (
+     member_id INT AUTO_INCREMENT PRIMARY KEY,
+     name VARCHAR(100) NOT NULL,
+     email VARCHAR(100) UNIQUE,
+     join_date DATE
 );
 
-
- 
+CREATE TABLE loan ( 
+    loan_id INT AUTO_INCREMENT PRIMARY KEY,
+    book_id INT,
+    member_id INT,
+    issue_date DATE,
+    return_date DATE,
+    FOREIGN KEY (book_id) REFERENCES book(book_id)
+            ON DELETE CASCADE
+            ON UPDATE CASCADE,
+	FOREIGN KEY (member_id) REFERENCES member(member_id)
+			      ON DELETE CASCADE
+            ON UPDATE CASCADE
+	);
